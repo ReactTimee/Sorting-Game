d4e465fc08df6a49b9450dc5c0cf8a7ca30b29c7
MarksZints <151776968+MarksZints@users.noreply.github.com>
2024-04-19 11:51:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
 var start = document.getElementById("start");
 var body = document.body;
+var inputs = document.createElement("input");
+var inputPoga = document.createElement("button");
+var poga1 = document.createElement("button");
+var poga2 = document.createElement("button");
+var poga3 = document.createElement("button");
+poga1.innerText = "vēsture";
+poga2.innerText = "sportisti";
+poga3.innerText = "pilsētas";
 start.addEventListener("click", vardaIevade);
 
 
 function vardaIevade() {
   start.classList.add("neredzams");
-  var inputs = document.createElement("input");
   body.appendChild(inputs);
-  var inputPoga = document.createElement("button");
   inputPoga.addEventListener("click", spelesIzvele)
   body.appendChild(inputPoga);
 }
 function spelesIzvele() {
   inputs.classList.add("neredzams");
   inputPoga.classList.add("neredzams");
-  var poga1 = document.createElement("button");
-  var poga2 = document.createElement("button");
-  var poga2 = document.createElement("button");
   poga1.addEventListener("click", vesture);
   poga2.addEventListener("click", sportisti);
-  poga3.addEventlistener("click", pilsetas);
+  poga3.addEventListener("click", pilsetas);
   body.appendChild(poga1);
   body.appendChild(poga2);
   body.appendChild(poga3);
+}
+function vesture() {
+  alert("sveiki");
+}
+function sportisti() {
+  alert("sveiki");
+}
+function pilsetas() {
+  alert("sveiki");
 }
